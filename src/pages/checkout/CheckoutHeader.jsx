2e8111e9390f3844ec2815c866eec32d30063313
Dicksonwh0217 <dicksonwh0217@gmail.com>
2025-11-13 16:40:27 +0800

    feat: Create add to cart feature

--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router';
 import './CheckoutHeader.css';
 
-export function CheckoutHeader() {
+export function CheckoutHeader({ paymentSummary }) {
     return (
         <>
             <div className="checkout-header">
@@ -15,7 +15,7 @@ export function CheckoutHeader() {
 
                     <div className="checkout-header-middle-section">
                         Checkout (<Link className="return-to-home-link"
-                            to="/">3 items</Link>)
+                            to="/">{paymentSummary.totalItems} items</Link>)
                     </div>
 
                     <div className="checkout-header-right-section">
--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -26,7 +26,7 @@ export function CheckOutPage({ cart }) {
             <link rel="icon" type="image" href="checkout.png" />
             <title>Checkout</title>
 
-            <CheckoutHeader />
+            <CheckoutHeader paymentSummary={paymentSummary} />
 
             <div className="checkout-page">
                 <div className="page-title">Review your order</div>
