9d5c496396886e8874e71b5115cac600b9741586
Dicksonwh0217 <dicksonwh0217@gmail.com>
2025-11-19 15:13:51 +0800

    feat(Orders): Add add to cart feature

--- src/pages/orders/OrdersGrid.jsx
+++ src/pages/orders/OrdersGrid.jsx
@@ -2,8 +2,9 @@ import { formatMoney } from '../../utils/money';
 import { Fragment } from 'react';
 import dayjs from 'dayjs';
 import { Link } from 'react-router';
+import axios from 'axios';
 
-export function OrdersGrid({ orders }) {
+export function OrdersGrid({ orders, loadCart }) {
     return (
         <div className="orders-grid">
             {orders.map((order) => {
@@ -30,6 +31,13 @@ export function OrdersGrid({ orders }) {
 
                         <div className="order-details-grid">
                             {order.products.map((orderProduct) => {
+                                const addToCart = async () => {
+                                    await axios.post('/api/cart-items', {
+                                        productId: orderProduct.product.id,
+                                        quantity: 1
+                                    });
+                                    await loadCart();
+                                }
                                 return (
                                     <Fragment key={orderProduct.product.id}>
                                         <div className="product-image-container">
@@ -46,9 +54,9 @@ export function OrdersGrid({ orders }) {
                                             <div className="product-quantity">
                                                 Quantity: {orderProduct.quantity}
                                             </div>
-                                            <button className="buy-again-button button-primary">
+                                            <button className="buy-again-button button-primary" onClick={addToCart}>
                                                 <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                                                <span className="buy-again-message">Add to Cart</span>
+                                                <span className="buy-again-message" >Add to Cart</span>
                                             </button>
                                         </div>
 
