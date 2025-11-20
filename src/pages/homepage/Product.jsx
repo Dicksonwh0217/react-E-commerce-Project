25ea96045f3d39f32c1722d5412b01d8c4c0b75f
Dicksonwh0217 <dicksonwh0217@gmail.com>
2025-11-20 17:27:33 +0800

    feat(Test): Create integration test for HomePage

--- src/pages/homepage/Product.jsx
+++ src/pages/homepage/Product.jsx
@@ -22,11 +22,15 @@ export function Product({ product, loadCart }) {
     }
 
     return (
-        <div className="product-container">
+        <div
+            className="product-container"
+            data-testid="product-container"
+        >
             <div className="product-image-container">
                 <img className="product-image"
                     data-testid="product-image"
-                    src={product.image} />
+                    src={product.image}
+                />
             </div>
 
             <div className="product-name limit-text-to-2-lines">
@@ -69,10 +73,10 @@ export function Product({ product, loadCart }) {
                 Added
             </div>
 
-            <button 
-            className="add-to-cart-button button-primary"
-            data-testid="add-to-cart-button"
-            onClick={addToCart}
+            <button
+                className="add-to-cart-button button-primary"
+                data-testid="add-to-cart-button"
+                onClick={addToCart}
             >
                 Add to Cart
             </button>
